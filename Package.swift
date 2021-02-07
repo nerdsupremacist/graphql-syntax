f4af46a5b11c33f917a4c827745cf5bf3862eb8f
Mathias Quintero <me@quintero.io>
2021-02-07 22:06:51 +0100

    removing platform requirements

--- Package.swift
+++ Package.swift
@@ -5,14 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "graphql-syntax",
-    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "GraphQLSyntax",
                  targets: ["GraphQLSyntax"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/nerdsupremacist/Syntax.git", from: "1.0.1"),
+        .package(url: "https://github.com/nerdsupremacist/Syntax.git", from: "1.0.2"),
     ],
     targets: [
         .target(
