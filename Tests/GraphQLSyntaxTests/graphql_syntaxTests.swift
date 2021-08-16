ac1690cda6ca2180a63640198f2a6a0d4b42e989
Mathias Quintero <me@quintero.io>
2021-08-16 11:09:36 +0200

    Add test for parsing boolean value

--- Tests/GraphQLSyntaxTests/graphql_syntaxTests.swift
+++ Tests/GraphQLSyntaxTests/graphql_syntaxTests.swift
@@ -28,6 +28,13 @@ final class graphql_syntaxTests: XCTestCase {
         print(String(data: try! JSONEncoder().encode(syntaxTree), encoding: .utf8)!)
     }
 
+    func testBooleanValue() {
+        let value = "false"
+        let parser = GraphQL.Value.Parser()
+        let parsed = try! parser.parse(value)
+        XCTAssertEqual(parsed, .bool(false))
+    }
+
     static var allTests = [
         ("testExample", testExample),
     ]
